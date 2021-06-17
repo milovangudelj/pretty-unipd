5dad876cc924f34e987dba5b576323b5e384bf61
Milo <milovan.gudelj@gmail.com>
2021-06-17 18:26:11 +0200

    Changed my info in script

--- inject.js
+++ inject.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Unipd overhaul
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  It changes the styling of every page on University of Padua's website
+// @author       Milovan Gudelj
 // @match        https://*.unipd.it/*
 // @icon         https://www.google.com/s2/favicons?domain=unipd.it
 // @grant        none
