0641b40d43770cff2a28e1232c95ac84507cac98
Milo <milovan.gudelj@gmail.com>
2021-06-18 22:59:16 +0200

    New version

--- inject.js
+++ inject.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Unipd overhaul
 // @namespace    http://tampermonkey.net/
-// @version      0.1.3
+// @version      0.1.4
 // @description  It changes the styling of every page on University of Padua's website
 // @author       Milovan Gudelj
 // @match        https://*.unipd.it/*
