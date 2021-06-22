a587131bd86974a008b2e399c6c694cfe2027e3e
Milo <milovan.gudelj@gmail.com>
2021-06-22 12:16:57 +0200

    Test for language change

--- scripts/loginPage.js
+++ scripts/loginPage.js
@@ -1,97 +1,107 @@
 // Fetch translations
-fetch("https://upo.milovangudelj.com/i18n/loginPage.json")
+let i18n = fetch("https://upo.milovangudelj.com/i18n/loginPage.json")
 	.then((response) => {
 		if (!response.ok) {
 			throw new Error("HTTP error " + response.status);
 		}
 		return response.json();
 	})
-	.then((json) => {
-		console.log(json);
-	})
+	.then((json) => json)
 	.catch((err) => console.error(err));
 
-// Get form reference
+// Old interface manipulation
 
-const uglyAssForm = document.querySelector(".form-horizontal");
-uglyAssForm.parentElement.className = "";
+const oldInterfaceManipulation = () => {
+	// Get form reference
 
-// Login form manipulation
+	const uglyAssForm = document.querySelector(".form-horizontal");
+	uglyAssForm.parentElement.className = "";
 
-uglyAssForm.children[1].style.display = "none"; // Hide radio buttons
-const submitButton = document.querySelector("div.col-xs-12 > #login_button_js"); // Get submit button
-uglyAssForm.insertBefore(submitButton, uglyAssForm.children[3]); // Move it outside
-uglyAssForm.removeChild(uglyAssForm.children[4]); // Remove old button wrapper
-submitButton.className = "my-submit-btn"; // Add my class name
-submitButton.removeAttribute("style"); // Removing default hidden styles
+	// Login form manipulation
 
-// Pull out form groups from their wrappers and hide them when done
-let formField = document.querySelector(
-	"span#div_hidden_before_js > div.form-group"
-);
-uglyAssForm.insertBefore(formField, uglyAssForm.children[0]);
-formField = document.querySelector("div#passwordbox > div.form-group");
-uglyAssForm.insertBefore(formField, uglyAssForm.children[1]);
-uglyAssForm.children[2].style.display = "none";
-uglyAssForm.children[4].style.display = "none";
+	uglyAssForm.children[1].style.display = "none"; // Hide radio buttons
+	const submitButton = document.querySelector(
+		"div.col-xs-12 > #login_button_js"
+	); // Get submit button
+	uglyAssForm.insertBefore(submitButton, uglyAssForm.children[3]); // Move it outside
+	uglyAssForm.removeChild(uglyAssForm.children[4]); // Remove old button wrapper
+	submitButton.className = "my-submit-btn"; // Add my class name
+	submitButton.removeAttribute("style"); // Removing default hidden styles
 
-// Pull out form input from unnecessary wrapper and add my class names
-let formInput = document.querySelector("div > input#j_username_js");
-let formGroup = formInput.parentElement.parentElement;
-formGroup.insertBefore(formInput, formGroup.children[1]);
-formGroup.removeChild(formGroup.children[2]);
-formGroup.className = "my-form-group";
-formInput.className = "my-form-control";
-formInput.setAttribute("placeholder", "nome.cognome@unipd.it");
-formInput.setAttribute("required", "");
-formGroup.children[0].className = "my-form-label";
-formGroup.children[0].innerHTML = 'Email <span class="required-field">*</span>';
+	// Pull out form groups from their wrappers and hide them when done
+	let formField = document.querySelector(
+		"span#div_hidden_before_js > div.form-group"
+	);
+	uglyAssForm.insertBefore(formField, uglyAssForm.children[0]);
+	formField = document.querySelector("div#passwordbox > div.form-group");
+	uglyAssForm.insertBefore(formField, uglyAssForm.children[1]);
+	uglyAssForm.children[2].style.display = "none";
+	uglyAssForm.children[4].style.display = "none";
 
-// Pull out form input from unnecessary wrapper and add my class names
-formInput = document.querySelector("div > input#password");
-formGroup = formInput.parentElement.parentElement;
-formGroup.insertBefore(formInput, formGroup.children[1]);
-formGroup.removeChild(formGroup.children[2]);
-formGroup.className = "my-form-group";
-formInput.className = "my-form-control";
-formInput.setAttribute("required", "");
-formGroup.children[0].className = "my-form-label";
-formGroup.children[0].innerHTML =
-	'Password <span class="required-field">*</span>';
+	// Pull out form input from unnecessary wrapper and add my class names
+	let formInput = document.querySelector("div > input#j_username_js");
+	let formGroup = formInput.parentElement.parentElement;
+	formGroup.insertBefore(formInput, formGroup.children[1]);
+	formGroup.removeChild(formGroup.children[2]);
+	formGroup.className = "my-form-group";
+	formInput.className = "my-form-control";
+	formInput.setAttribute("placeholder", "nome.cognome@unipd.it");
+	formInput.setAttribute("required", "");
+	formGroup.children[0].className = "my-form-label";
+	formGroup.children[0].innerHTML =
+		'Email <span class="required-field">*</span>';
 
-// Extract content from unnecessary wrappers
+	// Pull out form input from unnecessary wrapper and add my class names
+	formInput = document.querySelector("div > input#password");
+	formGroup = formInput.parentElement.parentElement;
+	formGroup.insertBefore(formInput, formGroup.children[1]);
+	formGroup.removeChild(formGroup.children[2]);
+	formGroup.className = "my-form-group";
+	formInput.className = "my-form-control";
+	formInput.setAttribute("required", "");
+	formGroup.children[0].className = "my-form-label";
+	formGroup.children[0].innerHTML =
+		'Password <span class="required-field">*</span>';
 
-document.body.insertBefore(
-	document.querySelector(
-		".col-md-offset-2.col-md-8.col-xs-offset-1.col-xs-10.well"
-	),
-	document.body.children[0]
-);
-document.body.removeChild(document.body.children[1]);
+	// Extract content from unnecessary wrappers
 
-// Remove default stylesheets
+	document.body.insertBefore(
+		document.querySelector(
+			".col-md-offset-2.col-md-8.col-xs-offset-1.col-xs-10.well"
+		),
+		document.body.children[0]
+	);
+	document.body.removeChild(document.body.children[1]);
 
-const defStyles = document.querySelector("style:first-of-type");
-const bootstrap = document.querySelectorAll('link[href*="bootstrap"]');
-document.head.removeChild(defStyles);
-bootstrap.forEach((el) => document.head.removeChild(el));
+	// Remove default stylesheets
 
-// Create placeholders
+	const defStyles = document.querySelector("style:first-of-type");
+	const bootstrap = document.querySelectorAll('link[href*="bootstrap"]');
+	document.head.removeChild(defStyles);
+	bootstrap.forEach((el) => document.head.removeChild(el));
 
-const oldInterface = document.createElement("div");
-oldInterface.setAttribute("class", "old-interface");
-const newInterface = document.createElement("div");
-newInterface.setAttribute("class", "new-interface");
+	// Create placeholders
 
-document.body.appendChild(oldInterface);
-document.body.appendChild(newInterface);
+	const oldInterface = document.createElement("div");
+	oldInterface.setAttribute("class", "old-interface");
+	const newInterface = document.createElement("div");
+	newInterface.setAttribute("class", "new-interface");
 
-oldInterface.insertBefore(
-	document.querySelector(
-		".col-md-offset-2.col-md-8.col-xs-offset-1.col-xs-10.well"
-	),
-	null
-);
+	document.body.appendChild(oldInterface);
+	document.body.appendChild(newInterface);
+
+	oldInterface.insertBefore(
+		document.querySelector(
+			".col-md-offset-2.col-md-8.col-xs-offset-1.col-xs-10.well"
+		),
+		null
+	);
+
+	// Hide crappy interface
+
+	oldInterface.style.setProperty("display", "none");
+	document.body.removeAttribute("class");
+};
 
 // Load my html fragment
 
@@ -107,6 +117,7 @@ $(document).ready(function () {
 let url = window.location.href;
 let hasParams = url.includes("?"); // Check if url has any parameters
 if (url.charAt(url.length - 1) === "#") url = url.slice(0, url.length - 2); // Remove # from end
+let currentLang = url.includes("lang=EN") ? "EN" : "IT";
 
 /** Sets the language selectors' links */
 const setLang = () => {
@@ -127,7 +138,7 @@ const setLang = () => {
 };
 
 /** Sets the correct link for a given language */
-const setUrl = (lang = "lang=IT") => {
+const setUrl = (lang = "IT") => {
 	let newUrl;
 
 	if (url.includes("lang")) {
@@ -140,7 +151,7 @@ const setUrl = (lang = "lang=IT") => {
 	return newUrl;
 };
 
-// Hide crappy interface
-
-oldInterface.style.setProperty("display", "none");
-document.body.removeAttribute("class");
+// i18n
+document.querySelector(".my-submit-btn").innerText = i18n.then(
+	(json) => json
+).form.loginBtn.currentLang;
