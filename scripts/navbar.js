31c93c0a700665c29dc58603ffbc5eb655b1295d
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-10-12 03:44:52 +0200

    Added await for fragment fetching

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,8 @@
 let navbar = document.querySelector("header");
 // navbar.parentElement.removeChild(navbar);
-let myNavbar = fetch("https://upo.milovangudelj.com/fragments/navbar.html");
+let myNavbar = await fetch(
+	"https://upo.milovangudelj.com/fragments/navbar.html"
+);
 let temp = document.createElement("template");
 temp.innerHTML = myNavbar.trim();
 
