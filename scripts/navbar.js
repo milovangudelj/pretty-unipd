1d80965c7798f9b0e91b50a8c6c599be7e4294b2
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-10-12 03:53:52 +0200

    I need to learn how to use fetch properly

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,12 +1,12 @@
 let navbar = document.querySelector("header");
 // navbar.parentElement.removeChild(navbar);
-let myNavbar = fetch(
-	"https://upo.milovangudelj.com/fragments/navbar.html"
-).then((res) => {
-	let temp = document.createElement("template");
-	temp.innerHTML = res.trim();
+let myNavbar = fetch("https://upo.milovangudelj.com/fragments/navbar.html")
+	.then((res) => res.text())
+	.then((txt) => {
+		let temp = document.createElement("template");
+		temp.innerHTML = txt.trim();
 
-	navbar.parentElement.replaceChild(temp.content.firstChild, navbar);
+		navbar.parentElement.replaceChild(temp.content.firstChild, navbar);
 
-	return res;
-});
+		return txt;
+	});
